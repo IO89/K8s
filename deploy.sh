df644cd1ef54056f7eb2532b6845f5849cc4d0db
IO89 <osipov.ivan.89@gmail.com>
2019-03-23 14:34:57 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t io89/multi-client:latest -t io89/multi-client:$SHA -f ./complex/client/Dockerfile ./complex/client/
+docker build -t io89/multi-server:latest -t io89/multi-server:$SHA -f ./complex/server/Dockerfile ./complex/server/
+docekr build -t io89/multi-worker:latest -t io89/multi-worker:$SHA -f ./complex/worker/Dockerfile ./complex/worker/
+
+docker push io89/multi-client:latest
+docker push io89/multi-server:latest
+docker push io89/multi-worker:latest
+
+docker push io89/multi-client:$SHA
+docker push io89/multi-server:$SHA
+docker push io89/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=io89/multi-server:$SHA
+kubectl set image deployments/client-deployment client=io89/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=io89/multi-worker:$SHA
